f545c4ab38dc1041e0cafa7b485d125039c99def
CorisMorri <103724309+CorisMorri@users.noreply.github.com>
2023-01-13 13:52:20 +0300

    ревью_1

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -4,20 +4,47 @@ import AddNewPointView from '../view/add-new-point-view.js';
 import {render} from '../render.js';
 export default class TripPresenter {
   #listContainer = new ListView();
+  #tripContainer = null;
+  #pointsModel = null;
+  #tripPoints = [];
   constructor({ tripContainer, pointsModel }) {
-    this.tripContainer = tripContainer;
-    this.pointsModel = pointsModel;
+    this.#tripContainer = tripContainer;
+    this.#pointsModel = pointsModel;
   }
 
   init() {
 
-    this.tripPoints = [...this.pointsModel.getPoints()];
+    this.#tripPoints = [...this.#pointsModel.points];
 
-    render(this.#listContainer, this.tripContainer);
-    render(new AddNewPointView(this.tripPoints[1]), this.#listContainer.getElement());
+    render(this.#listContainer, this.#tripContainer);
+    //render(new AddNewPointView(this.#tripPoints[1]), this.#listContainer.element);
 
-    for (let i = 0; i < this.tripPoints.length; i++) {
-      render(new PointView(this.tripPoints[i]), this.#listContainer.getElement());
+    for (let i = 0; i < this.#tripPoints.length; i++) {
+      this.#renderPoint(this.#tripPoints[i]);
+      //render(new PointView(this.#tripPoints[i]), this.#listContainer.element);
     }
+  }
+
+  #renderPoint(point) {
+    const listComponent = new PointView({ point });
+    const listEditComponent = new AddNewPointView({point});
+
+    const replaceElementToForm = () => {
+      this.#listContainer.element.replaceChild(listEditComponent.element, listComponent.element);
+    };
+    const replaceFormToElement = () => {
+      this.#listContainer.element.replaceChild(listComponent.element, listEditComponent.element);
+    };
+
+    listComponent.element.querySelector('.trip-events__item').addEventListener('click', () => {
+      replaceElementToForm();
+    });
+
+    listEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      replaceFormToElement();
+    });
+
+    render(listComponent, this.#listContainer.element);
   }
 }
--- src/view/loading-view.js
+++ src/view/loading-view.js
@@ -5,19 +5,22 @@ function createLoadingTemplate() {
 }
 
 export default class LoandingView {
-  getTemplate() {
+
+  #element = null;
+
+  get template() {
     return createLoadingTemplate();
   }
 
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
     }
 
-    return this.element;
+    return this.#element;
   }
 
   removeElement() {
-    this.element = null;
+    this.#element = null;
   }
 }
